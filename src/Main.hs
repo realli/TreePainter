b4d0604a74f8f5a59683b4168fc6d54762825370
leo <lrlanswer@gmail.com>
2014-10-27 20:21:24 +0800

    a working program

--- src/Main.hs
+++ src/Main.hs
@@ -1,3 +1,15 @@
-import qualified Painting as P
+import Painting (convertTree2Diagram)
+import Parsing (parseTree)
+import Diagrams.Backend.SVG.CmdLine
+import Diagrams.Prelude
+import Data.Monoid (mempty)
 
-main = P.main
+main = mainWith $ \file -> do
+            f <- readFile file
+            case parseTree f of
+                Left s -> do 
+                    putStrLn $ show s
+                    return mempty
+                Right tree -> return (convertTree2Diagram tree)
+
+            
--- src/Painting.hs
+++ src/Painting.hs
@@ -1,5 +1,5 @@
 {-# LANGUAGE NoMonomorphismRestriction #-}
-module Painting where
+module Painting (convertTree2Diagram) where
 
 import Diagrams.Prelude
 import Diagrams.Backend.SVG.CmdLine
@@ -10,15 +10,15 @@ type Label = Integer
 type Graph = Diagram B R2
 
 -- paint one node with name in it
-node :: (Show a) => a -> Label -> Graph
-node n l = text (show n) # fontSizeN 0.1 # fc red
+node :: String -> Label -> Graph
+node n l = text n # fontSizeN 0.1 # fc red
            <> circle 0.2 # fc green # named l
 
 -- convert Tree to tuple of Graph and Label
 -- take an Label as initial, return the next availble Label also
 -- Label is used to name graph node, so we can draw line between
 -- each tree nodes
-paintTree :: (Show a) => Tree a -> Label -> (Graph, Label)
+paintTree :: Tree String -> Label -> (Graph, Label)
 paintTree (Branch value coord leaves) start =
                             foldl go (paintNode, start + 1) leaves
     where paintNode = node value start # moveTo (p2 coord)
@@ -28,8 +28,6 @@ paintTree (Branch value coord leaves) start =
                                       fst elePainting <> ms, snd elePainting)
 
 -- convert Tree to Graph
-convertTree2Diagram :: (Show a) => Tree a -> Graph
+convertTree2Diagram :: Tree String -> Graph
 convertTree2Diagram tr = let (g, _) = paintTree tr 1
                          in g
-
-main = mainWith (convertTree2Diagram $ Branch 1 (-2,-2) [(Branch 2 (-3,-3) []), (Branch 3 (-1,-3) [])])
